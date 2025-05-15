74e4eaca325234655a6a462b63146ad49a0fa87d
Jo3-h <joe.hosking1@gmail.com>
2025-05-15 18:57:17 +0930

    completed impementation of scanner class and token class

--- Ember.java
+++ Ember.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters.ember;
+// Ember.java
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,7 +10,15 @@ import java.util.List;
 
 public class Ember {
 
-    static boolean hadError = false;
+    static boolean hadError = false; // boolean variable to indicate whether an error has been hit in the program execution
+
+    /* Function: main() - Entrypoint to Ember Interpreter main()
+     * 
+     * @param *.ember script to be executed or void
+     * @return void
+     * 
+     * Purpose of this function is to take the commandline arguments and either run the specified Ember Script or enter and interactive session
+     */
     public static void main(String[] args) throws IOException {
         if (args.length > 1){
             System.out.printLn("Usage: Ember [script]");
@@ -22,6 +30,13 @@ public class Ember {
         }
     }
 
+    /* Function: runFile() - Execution of Ember code in specified file path
+     * 
+     * @param path to *.ember file containing ember program
+     * @return void
+     * 
+     * This function is called in the main function when a file argument is passed, therefore the Interpreter should execute the ember code stored in this file location
+     */
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
@@ -30,6 +45,13 @@ public class Ember {
         if (hadError) System.exit(65);
     }
 
+    /* Function: runPrompt() - Interactive session of the Ember Interpreter
+     * 
+     * @param void
+     * @return void
+     * 
+     * This function has an infinite control loop to maintain the session until the user specifies the end of the interactive session with null input
+     */
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
@@ -43,6 +65,11 @@ public class Ember {
         }
     }
 
+    /* Function: run() - Execute a line of code in the Ember programming language
+     * 
+     * @param source 
+     * @return void
+     */
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
